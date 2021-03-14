a3f050a155bfbc08b9d7c507a7f7e6bfef81bba0
Patrick DeYoreo <pdeyoreo@gmail.com>
2021-03-13 21:53:44 -0800

    Reverted provided to original

--- crypto/provided/_print_hex_buffer.c
+++ crypto/provided/_print_hex_buffer.c
@@ -3,16 +3,15 @@
 #include <stdio.h>
 
 /**
- * _print_hex_buffer - print a hexadecimal representation of a buffer
+ * _print_hex_buffer - Prints a buffer in its hexadecimal form
  *
- * @buf: pointer to the buffer to be printed
- * @len: number of bytes to be printed
+ * @buf: Pointer to the buffer to be printed
+ * @len: Number of bytes from @buf to be printed
  */
 void _print_hex_buffer(uint8_t const *buf, size_t len)
 {
-	if (buf)
-	{
-		while (len--)
-			printf("%02x", *buf++);
-	}
+	size_t i;
+
+	for (i = 0; buf && i < len; i++)
+		printf("%02x", buf[i]);
 }
