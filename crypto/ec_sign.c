674e696181bfbd568a081a2cebc108a428aed1a8
Patrick DeYoreo <pdeyoreo@gmail.com>
2021-03-14 23:22:24 -0700

    Adding tasks 6 and 7 - ec_sign and ec_verify

---
+++ crypto/ec_sign.c
@@ -0,0 +1,28 @@
+#include "hblk_crypto.h"
+
+/**
+ * ec_sign - sign a message using an EC private key
+ *
+ * @key: pointer to the EC key pair containing the private key to be used
+ * @msg: message to be signed
+ * @msglen: length of @message
+ * @sig: address at which to store the signature
+ *
+ * Return: If key or msg is NULL, or if an error occurs, return NULL.
+ * Otherwise, return a pointer to the buffer containing the signature.
+ */
+uint8_t *ec_sign(
+	EC_KEY const *key, uint8_t const *msg, size_t msglen, sig_t *sig)
+{
+	unsigned int len = sig->len;
+
+	if (key && msg)
+	{
+		if (ECDSA_sign(0, msg, msglen, sig->sig, &len, (EC_KEY *)key))
+		{
+			sig->len = len;
+			return (sig->sig);
+		}
+	}
+	return (NULL);
+}
