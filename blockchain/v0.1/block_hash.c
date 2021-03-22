fee179588397d04ce277bf45dd575fa3a356e438
Patrick DeYoreo <pdeyoreo@gmail.com>
2021-03-21 19:52:15 -0700

    Removed extra space

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -15,5 +15,5 @@
 uint8_t *block_hash(
 	block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
-	return (sha256((int8_t const *) block, sizeof(*block), hash_buf));
+	return (sha256((int8_t const *)block, sizeof(*block), hash_buf));
 }
