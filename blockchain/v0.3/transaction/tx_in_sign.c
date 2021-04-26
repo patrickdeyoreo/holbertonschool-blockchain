49bf8faa4ecfe8f17d03205b61cf1924171b93e5
Patrick DeYoreo <pdeyoreo@gmail.com>
2021-04-25 23:16:53 -0700

    Adding old tx_in_sign

--- blockchain/v0.3/transaction/tx_in_sign.c
+++ blockchain/v0.3/transaction/tx_in_sign.c
@@ -19,28 +19,19 @@ sig_t *tx_in_sign(
 	uint8_t pub[EC_PUB_LEN] = {0};
 	ssize_t index = 0;
 	unspent_tx_out_t *unspent_tx_out = NULL;
-	int all_unspent_count = llist_size(all_unspent);
 
-	if (all_unspent_count == -1)
-		return (NULL);
-
-	while (index < all_unspent_count)
+	while (index < llist_size(all_unspent))
 	{
 		unspent_tx_out = llist_get_node_at(all_unspent, index);
 		if (!memcmp(in->tx_out_hash, unspent_tx_out->out.hash,
 				SHA256_DIGEST_LENGTH))
-		{
 			break;
-		}
 		unspent_tx_out = NULL;
 		index += 1;
 	}
 
-	if (!unspent_tx_out)
-		return (NULL);
-	if (!ec_to_pub(sender, pub))
-		return (NULL);
-	if (memcmp(pub, unspent_tx_out->out.pub, EC_PUB_LEN))
+	if (!unspent_tx_out || !ec_to_pub(sender, pub) ||
+		memcmp(pub, unspent_tx_out->out.pub, EC_PUB_LEN))
 		return (NULL);
 	if (!ec_sign(sender, tx_id, SHA256_DIGEST_LENGTH, &in->sig))
 		return (NULL);
