fae48340939d6d8fce5e92cd5b6323c57a8bb303
Patrick DeYoreo <pdeyoreo@gmail.com>
2021-04-25 23:18:05 -0700

    Fixed betty style issues

--- blockchain/v0.3/transaction/tx_in_sign.c
+++ blockchain/v0.3/transaction/tx_in_sign.c
@@ -6,7 +6,7 @@
  * @in: pointer to the transaction input structure to sign
  * @tx_id: buffer containing the ID of the transaction in which @in is stored
  * @sender: buffer containing the private key of the receiver of the coins in
- * 	    the transaction output referenced by the transaction input
+ *          the transaction output referenced by the transaction input
  * @all_unspent: list of all unspent transaction outputs
  *
  * Return: If an error occurs, return NULL.
@@ -19,19 +19,28 @@ sig_t *tx_in_sign(
 	uint8_t pub[EC_PUB_LEN] = {0};
 	ssize_t index = 0;
 	unspent_tx_out_t *unspent_tx_out = NULL;
+	int all_unspent_count = llist_size(all_unspent);
 
-	while (index < llist_size(all_unspent))
+	if (all_unspent_count == -1)
+		return (NULL);
+
+	while (index < all_unspent_count)
 	{
 		unspent_tx_out = llist_get_node_at(all_unspent, index);
 		if (!memcmp(in->tx_out_hash, unspent_tx_out->out.hash,
 				SHA256_DIGEST_LENGTH))
+		{
 			break;
+		}
 		unspent_tx_out = NULL;
 		index += 1;
 	}
 
-	if (!unspent_tx_out || !ec_to_pub(sender, pub) ||
-		memcmp(pub, unspent_tx_out->out.pub, EC_PUB_LEN))
+	if (!unspent_tx_out)
+		return (NULL);
+	if (!ec_to_pub(sender, pub))
+		return (NULL);
+	if (memcmp(pub, unspent_tx_out->out.pub, EC_PUB_LEN))
 		return (NULL);
 	if (!ec_sign(sender, tx_id, SHA256_DIGEST_LENGTH, &in->sig))
 		return (NULL);
