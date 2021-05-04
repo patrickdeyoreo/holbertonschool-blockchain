ad520a3b63b78bcae86bd834dd2a8f6850604ef0
Patrick DeYoreo <pdeyoreo@gmail.com>
2021-05-04 00:15:57 -0700

    Implemented info command

--- cli/cli_info.c
+++ cli/cli_info.c
@@ -18,5 +18,11 @@ int cli_info(state_t *state)
 		fprintf(stderr, "%s: too many arguments\n", state->argv[0]);
 		return ((state->status = 2));
 	}
+	fprintf(stdout, "Blocks:  %d\n",
+		llist_size(state->blockchain->chain));
+	fprintf(stdout, "UTXOs:   %d\n",
+		llist_size(state->blockchain->unspent));
+	fprintf(stdout, "TX Pool: %d\n",
+		llist_size(state->tx_pool));
 	return ((state->status = EXIT_SUCCESS));
 }
