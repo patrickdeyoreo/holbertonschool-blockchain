b2a2709b25eeb8b049e4e2232c5f7b302fd63db3
Patrick DeYoreo <pdeyoreo@gmail.com>
2022-02-28 14:35:36 -0800

    Added uninstall script for llist shared library

---
+++ llist/uninstall.sh
@@ -0,0 +1,25 @@
+#!/usr//bin/env sh
+
+PROGNAME="${0##*/}"
+
+if test "$(id -u)" -ne 0
+then
+  >&2 printf '%s: must be executed as root\n' "${PROGNAME}"
+  exit 1
+fi
+
+printf 'Uninstalling library and headers ...\n'
+rm -f /usr/local/lib/libllist.so
+rm -f  /usr/local/include/llist.h
+ldconfig
+
+printf 'Uninstalling man pages ...\n'
+for relpath in man/*.3
+do
+  basename="${relpath##*/}"
+  rm -f "/usr/local/man/man3/${basename}"
+done
+mandb
+
+echo "Complete!"
+exit 0
