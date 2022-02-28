b4b9616d5740861d3b1ddf31b32cb9e8577a9651
Patrick DeYoreo <pdeyoreo@gmail.com>
2022-02-28 12:43:23 -0800

    Updated install.sh

--- llist/install.sh
+++ llist/install.sh
@@ -1,19 +1,22 @@
 #!/usr//bin/env sh
 
+PROGNAME="${0##*/}"
+
 if test "$(id -u)" -ne 0
 then
-  >&2 echo "Must be executed as root"
+  >&2 printf '%s: must be executed as root\n' "${PROGNAME}"
   exit 1
 fi
 
-echo "Installing library and headers ..."
+printf 'Installing library and headers ...\n'
 cp libllist.so /usr/local/lib/
 cp llist.h /usr/local/include/
 ldconfig
 
-echo "Installing man pages ..."
+printf 'Installing man pages ...\n'
 mkdir -p /usr/local/man/man3
 cp man/*.3 /usr/local/man/man3/
 mandb
 
 echo "Complete!"
+exit 0
